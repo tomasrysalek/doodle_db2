835b1e56a0f4c7cbb396470babb7e0fe74315bff
tomas.rysalek <tomas.rysalek@gmail.com>
2019-03-30 23:51:04 +0100

    wooohooo

--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,3 @@ import ReactDOM from "react-dom";
 import Login from './components/loginForm/login';
 
 ReactDOM.render(<Login />,document.getElementById("login"));
-
-
--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 import express from 'express';
+import connect from './config/db';
+
 
 const app = express();
 const port = process.env.PORT || 4433;
+
+connect
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 app.listen(port, () => {
     console.log(`Woohooooo`)
 });
 
 app.get('/test',(req,res)=>{
-    res.send({express:'Wooohooooo'});
+    res.send(req.body.email)
 });
