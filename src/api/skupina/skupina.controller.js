5738c7dc3366873d3af21b756a1c2b93f44c688c
Tomas Rysalek <tomas.rysaleke@gmail.com>
2019-05-05 22:57:58 +0200

    refactor pro pridani akce pro skupiny, added pridani uzivatele do skupiny, ziskani akci pro skupinu

--- src/api/skupina/skupina.controller.js
+++ src/api/skupina/skupina.controller.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import skupinaService from './skupina.service';
 import passport from 'passport';
+import skupina from './skupina.model';
 const router = express.Router();
 
 //Vytvor skupinu
@@ -8,4 +9,5 @@ router.post('/create',passport.authenticate('jwt',{session:false}),skupinaServic
 //Pridej noveho clena skupiny
 router.post('/adduser',skupinaService.adduser);
 router.get('/getsk',passport.authenticate('jwt',{session:false}),skupinaService.getAll);
+router.get('/getAll',skupinaService.getUdalosti);
 export default router;
