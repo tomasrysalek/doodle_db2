660773cfdc558953f23b6bbff65f6ecc10dd15a0
tomas.rysalek <tomas.rysalek@gmail.com>
2019-05-30 18:50:16 +0200

    Chat
    Budu potřebovat aby jsi mi posílal name: (username uživatele) a room(jméno skupiny) v headu stejně jako token

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ import skupinaRouter from './src/api/skupina/skupina.controller';
 import udalostRouter from './src/api/udalost/udalost.controller';
 const app = express();
 const port = process.env.PORT || 4433;
+app.set('view engine','ejs')
+
+app.use(express.static('public'));
 
 connect
   .authenticate()
@@ -15,16 +18,41 @@ connect
   })
   .catch(err => {
     console.error('Unable to connect to the database:', err);
-  });
-  
-  app.use(cors());
-  app.use(bodyParser.json());
-  app.use('/udalost',udalostRouter);
-  app.use('/user', userRouter);
-  app.use('/skupina',skupinaRouter);
-
-app.listen(port, () => {
-    console.log(`Woohooooo`)
 });
+  
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/udalost',udalostRouter);
+app.use('/user', userRouter);
+app.use('/skupina',skupinaRouter);
+
+
+const server = app.listen(port)
+const io = require("socket.io")(server)
+
+app.get('/',(req,res) => {
+  console.log('room:', req.headers.room)
+  console.log('name:', req.headers.name)
+  // res.render('index',{room: req.body.room})
+  io.on('connection',(socket) => {
+
+    socket.removeAllListeners()
+    socket.join(req.headers.room)    
+    //Set username
+    socket.username = req.headers.name
+  
+      //listen on new_message
+      socket.on('new_message', (data) => {
+          //broadcast the new message
+          io.to(req.headers.room).emit('new_message', {message : data.message, username : socket.username});
+      })
+
+      //listen on typing
+      socket.on('typing', (data) => {
+        socket.to(req.headers.room).broadcast.emit('typing', {username : socket.username})
+      })
+  })
+})
+
 
 
